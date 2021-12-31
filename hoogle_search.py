605592a12707ea3de363a20d7b1327010302ae4f
Penny Chase <mpchase@mac.com>
2021-12-30 23:52:47 -0500

    Fixed HoogleSearch

--- hoogle_search.py
+++ hoogle_search.py
@@ -2,10 +2,13 @@
 
 import sublime, sublime_plugin, webbrowser
 import urllib.request, json
+from html.parser import HTMLParser
 import pprint as pp
 
 # Global varz
 results = [];
+
+
 
 class HoogleSearchCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -17,15 +20,17 @@ def search(inp):
 	query   = urllib.parse.quote_plus(inp)
 	url     = "https://www.haskell.org/hoogle/?hoogle="+ query +"&mode=json"
 	data    = urllib.request.urlopen(url).read().decode()
-	data 	= json.loads(data)
-	results = data['results']
+	results = json.loads(data)
+	# results = data['results']
 
 
 	formatedResult = []
 
 	if results:
 		for result in results:
-			formatedResult.append(result['self'])
+			formatted = format(result)
+			if formatted != '':
+				formatedResult.append(formatted)
 	else:
 		formatedResult.append("No results")
 
@@ -34,4 +39,38 @@ def search(inp):
 
 def on_done(index):
 	if index != -1:
-		webbrowser.open(results[index]['location'], 2)
+		webbrowser.open(results[index]['url'], 2)
+
+
+def format (result):
+	if result.get('module') == None:
+		return ''
+	else:
+		modname = result.get('module').get('name')
+		if modname == None:
+			return ''
+	if result.get('item') == None:
+		return ''
+	else:
+		res = result['item'].split('::')
+		name = removeHtml(res[0])
+		if len (res) == 1:
+			ret = modname + ' ' + name
+		else:
+			ret = modname + ' ' + name + '::' + res[1]
+	return (HTMLParser().unescape (ret))
+
+def removeHtml (str):
+	result = ''
+	inHtml = False
+	for c in str:
+		if c == '<':
+			inHtml = True
+		elif c == '>':
+			inHtml = False
+		else:
+			if not inHtml:
+				result = result + c
+	return (result)
+
+
